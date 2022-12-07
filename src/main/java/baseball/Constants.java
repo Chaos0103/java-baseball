8e72d0a5123d9e1a493e91da12adeabdf87590ed
chaos0103 <lyt1228@naver.com>
2022-12-07 17:55:52 +0900

    feat(baseball): 1부터 9까지 서로 다른 수 3자리를 생성하는 기능 구현

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public abstract class Constants {
+    public static final int MAX_RANGE = 9;
+    public static final int MIN_RANGE = 1;
+    public static final int SIZE = 3;
+}
--- src/main/java/baseball/NumberGenerator.java
+++ src/main/java/baseball/NumberGenerator.java
@@ -1,10 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static baseball.Constants.*;
 
 public class NumberGenerator {
 
     public List<Integer> createRandomNumbers() {
-        return null;
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
